b36fb65416f9d89622b8479c3afdc68ad048a700
Tom Gontard <tomgontard@protonmail.com>
2025-07-27 11:26:07 +0200

    Adding new items in the shop

--- src/config/items.js
+++ src/config/items.js
@@ -1,50 +1,787 @@
-// src/config/items.js
 export default [
-  // consumables
   {
-    id: 'healPotion',
-    type: 'consumable',
-    name: 'Health Potion',
-    cost: 100,
-    stats: {},            // no persistent stats
-    effect: 'restore_hp', // handled in code
+    "id": "healPotion20",
+    "type": "consumable",
+    "name": "Small Healing Potion",
+    "cost": 100,
+    "stats": {},
+    "effect": "restore_hp_20"
   },
   {
-    id: 'resetPotion',
-    type: 'consumable',
-    name: 'Attribute Reset Potion',
-    cost: 1000,
-    stats: {},
-    effect: 'reset_attributes'
-  },
-
-  // equipment
-  {
-    id: 'iron_sword',
-    type: 'equipment',
-    category: 'weapon',
-    name: 'Iron Sword',
-    rarity: 'common',
-    cost: 300,
-    stats: { strength: 2 }
+    "id": "healPotion50",
+    "type": "consumable",
+    "name": "Medium Healing Potion",
+    "cost": 200,
+    "stats": {},
+    "effect": "restore_hp_50"
   },
   {
-    id: 'leather_helmet',
-    type: 'equipment',
-    category: 'helmet',
-    name: 'Leather Helmet',
-    rarity: 'common',
-    cost: 200,
-    stats: { vitality: 1 }
+    "id": "healPotion100",
+    "type": "consumable",
+    "name": "Large Healing Potion",
+    "cost": 300,
+    "stats": {},
+    "effect": "restore_hp_100"
   },
   {
-    id: 'iron_chestplate',
-    type: 'equipment',
-    category: 'chest',
-    name: 'Iron Chestplate',
-    rarity: 'uncommon',
-    cost: 500,
-    stats: { vitality: 3 }
+    "id": "resetPotion",
+    "type": "consumable",
+    "name": "Attribute Reset Potion",
+    "cost": 1000,
+    "stats": {},
+    "effect": "reset_attributes"
   },
-  // … add more items here …
+  {
+    "id": "common_helmet_1",
+    "type": "equipment",
+    "category": "helmet",
+    "name": "Visage of Shadows",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "strength": 1,
+      "wisdom": 1,
+      "agility": 1
+    }
+  },
+  {
+    "id": "common_chest_2",
+    "type": "equipment",
+    "category": "chest",
+    "name": "Steel Vest",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "wisdom": 1,
+      "agility": 1,
+      "luck": 1
+    }
+  },
+  {
+    "id": "common_boots_3",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Shadow Striders",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "intelligence": 1,
+      "agility": 2
+    }
+  },
+  {
+    "id": "common_boots_4",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Boots of Achilles",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "wisdom": 1,
+      "intelligence": 1,
+      "vitality": 1
+    }
+  },
+  {
+    "id": "common_leggings_5",
+    "type": "equipment",
+    "category": "leggings",
+    "name": "Shadowstep Leggings",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "intelligence": 2,
+      "luck": 1,
+      "vitality": 1
+    }
+  },
+  {
+    "id": "common_boots_6",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Boots of Hermes",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "luck": 1,
+      "wisdom": 1,
+      "intelligence": 1
+    }
+  },
+  {
+    "id": "common_helmet_7",
+    "type": "equipment",
+    "category": "helmet",
+    "name": "Helm of the Guardian",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "strength": 1,
+      "wisdom": 2
+    }
+  },
+  {
+    "id": "common_chest_8",
+    "type": "equipment",
+    "category": "chest",
+    "name": "Vest of the Seer",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "wisdom": 1,
+      "luck": 1
+    }
+  },
+  {
+    "id": "common_boots_9",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Storm Treads",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "wisdom": 1,
+      "intelligence": 1
+    }
+  },
+  {
+    "id": "common_chest_10",
+    "type": "equipment",
+    "category": "chest",
+    "name": "Omen's Armor",
+    "rarity": "common",
+    "cost": 500,
+    "stats": {
+      "intelligence": 2,
+      "luck": 1,
+      "wisdom": 1
+    }
+  },
+  {
+    "id": "uncommon_boots_1",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Greaves of the Undying",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "strength": 4,
+      "wisdom": 1,
+      "vitality": 2,
+      "luck": 2
+    }
+  },
+  {
+    "id": "uncommon_leggings_2",
+    "type": "equipment",
+    "category": "leggings",
+    "name": "Greaves of Titans",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "luck": 4,
+      "wisdom": 2,
+      "agility": 2,
+      "strength": 4
+    }
+  },
+  {
+    "id": "uncommon_weapon_3",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Frostmourne",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "intelligence": 2,
+      "vitality": 2,
+      "agility": 3,
+      "strength": 2,
+      "luck": 1
+    }
+  },
+  {
+    "id": "uncommon_gloves_4",
+    "type": "equipment",
+    "category": "gloves",
+    "name": "Handwraps of Precision",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "wisdom": 2,
+      "agility": 2,
+      "intelligence": 2,
+      "luck": 3,
+      "vitality": 1
+    }
+  },
+  {
+    "id": "uncommon_boots_5",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Boots of the Wanderer",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "vitality": 1,
+      "agility": 2,
+      "wisdom": 2,
+      "intelligence": 1,
+      "luck": 1
+    }
+  },
+  {
+    "id": "uncommon_weapon_6",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Jade Blade",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "agility": 2,
+      "vitality": 2,
+      "strength": 1,
+      "wisdom": 1,
+      "intelligence": 1
+    }
+  },
+  {
+    "id": "uncommon_weapon_7",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Lightbringer",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "luck": 4,
+      "vitality": 2,
+      "agility": 2,
+      "wisdom": 1,
+      "strength": 2
+    }
+  },
+  {
+    "id": "uncommon_weapon_8",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Blade of the Phoenix",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "wisdom": 1,
+      "strength": 2,
+      "luck": 1,
+      "vitality": 1,
+      "intelligence": 2,
+      "agility": 1
+    }
+  },
+  {
+    "id": "uncommon_gloves_9",
+    "type": "equipment",
+    "category": "gloves",
+    "name": "Gloves of the Oracle",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "strength": 1,
+      "agility": 3,
+      "intelligence": 2,
+      "vitality": 1,
+      "wisdom": 2
+    }
+  },
+  {
+    "id": "uncommon_boots_10",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Greaves of Stability",
+    "rarity": "uncommon",
+    "cost": 1000,
+    "stats": {
+      "vitality": 1,
+      "agility": 4,
+      "intelligence": 3,
+      "wisdom": 2,
+      "luck": 1
+    }
+  },
+  {
+    "id": "rare_leggings_1",
+    "type": "equipment",
+    "category": "leggings",
+    "name": "Wraithwalkers",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "luck": 4,
+      "vitality": 3,
+      "strength": 3,
+      "intelligence": 6,
+      "agility": 3,
+      "wisdom": 1
+    }
+  },
+  {
+    "id": "rare_chest_2",
+    "type": "equipment",
+    "category": "chest",
+    "name": "Dragon Scale Armor",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "agility": 5,
+      "vitality": 5,
+      "intelligence": 3,
+      "strength": 3,
+      "wisdom": 3
+    }
+  },
+  {
+    "id": "rare_weapon_3",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Axe of the Ancients",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "strength": 3,
+      "intelligence": 5,
+      "vitality": 3,
+      "wisdom": 3,
+      "luck": 4,
+      "agility": 1
+    }
+  },
+  {
+    "id": "rare_gloves_4",
+    "type": "equipment",
+    "category": "gloves",
+    "name": "Gloves of the Outcast",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "agility": 5,
+      "wisdom": 3,
+      "strength": 3,
+      "intelligence": 5,
+      "luck": 4,
+      "vitality": 2
+    }
+  },
+  {
+    "id": "rare_leggings_5",
+    "type": "equipment",
+    "category": "leggings",
+    "name": "Legplates of the Titan",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "wisdom": 2,
+      "intelligence": 5,
+      "strength": 2,
+      "agility": 6,
+      "luck": 1,
+      "vitality": 4
+    }
+  },
+  {
+    "id": "rare_gloves_6",
+    "type": "equipment",
+    "category": "gloves",
+    "name": "Magefist Gloves",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "intelligence": 6,
+      "strength": 3,
+      "vitality": 7,
+      "wisdom": 1,
+      "agility": 1,
+      "luck": 1
+    }
+  },
+  {
+    "id": "rare_gloves_7",
+    "type": "equipment",
+    "category": "gloves",
+    "name": "Gloves of the Gambler",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "wisdom": 3,
+      "vitality": 3,
+      "intelligence": 5,
+      "luck": 2,
+      "strength": 8,
+      "agility": 1
+    }
+  },
+  {
+    "id": "rare_helmet_8",
+    "type": "equipment",
+    "category": "helmet",
+    "name": "Crown of Valor",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "strength": 2,
+      "wisdom": 4,
+      "intelligence": 6,
+      "agility": 3,
+      "luck": 1,
+      "vitality": 3
+    }
+  },
+  {
+    "id": "rare_leggings_9",
+    "type": "equipment",
+    "category": "leggings",
+    "name": "Pants of Prosperity",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "wisdom": 4,
+      "vitality": 4,
+      "strength": 2,
+      "agility": 3,
+      "luck": 2,
+      "intelligence": 1
+    }
+  },
+  {
+    "id": "rare_boots_10",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Shadow Striders",
+    "rarity": "rare",
+    "cost": 2500,
+    "stats": {
+      "agility": 5,
+      "luck": 8,
+      "vitality": 5,
+      "strength": 3,
+      "wisdom": 1
+    }
+  },
+  {
+    "id": "epic_chest_1",
+    "type": "equipment",
+    "category": "chest",
+    "name": "Robes of the Magi",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "luck": 7,
+      "strength": 2,
+      "vitality": 7,
+      "wisdom": 5,
+      "intelligence": 4,
+      "agility": 1
+    }
+  },
+  {
+    "id": "epic_helmet_2",
+    "type": "equipment",
+    "category": "helmet",
+    "name": "Spectral Hood",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "agility": 8,
+      "strength": 6,
+      "vitality": 5,
+      "luck": 5,
+      "intelligence": 5,
+      "wisdom": 4
+    }
+  },
+  {
+    "id": "epic_boots_3",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Boots of the Swift",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "agility": 9,
+      "vitality": 3,
+      "luck": 7,
+      "strength": 6,
+      "wisdom": 5,
+      "intelligence": 3
+    }
+  },
+  {
+    "id": "epic_helmet_4",
+    "type": "equipment",
+    "category": "helmet",
+    "name": "Helm of Insight",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "agility": 6,
+      "wisdom": 3,
+      "intelligence": 6,
+      "strength": 7,
+      "luck": 6,
+      "vitality": 6
+    }
+  },
+  {
+    "id": "epic_leggings_5",
+    "type": "equipment",
+    "category": "leggings",
+    "name": "Legguards of Swiftness",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "intelligence": 3,
+      "agility": 4,
+      "wisdom": 7,
+      "vitality": 7,
+      "strength": 3,
+      "luck": 3
+    }
+  },
+  {
+    "id": "epic_boots_6",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Nightstalker Boots",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "wisdom": 9,
+      "intelligence": 1,
+      "strength": 4,
+      "vitality": 4,
+      "luck": 3,
+      "agility": 2
+    }
+  },
+  {
+    "id": "epic_gloves_7",
+    "type": "equipment",
+    "category": "gloves",
+    "name": "Gauntlets of the Phoenix",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "wisdom": 3,
+      "intelligence": 13,
+      "agility": 3,
+      "strength": 5,
+      "luck": 7,
+      "vitality": 5
+    }
+  },
+  {
+    "id": "epic_weapon_8",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Shadow Reaper",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "vitality": 9,
+      "strength": 3,
+      "agility": 5,
+      "luck": 6,
+      "intelligence": 6,
+      "wisdom": 2
+    }
+  },
+  {
+    "id": "epic_chest_9",
+    "type": "equipment",
+    "category": "chest",
+    "name": "Armor of the Ancients",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "strength": 4,
+      "wisdom": 6,
+      "luck": 3,
+      "vitality": 6,
+      "agility": 5,
+      "intelligence": 8
+    }
+  },
+  {
+    "id": "epic_boots_10",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Storm Treads",
+    "rarity": "epic",
+    "cost": 5000,
+    "stats": {
+      "agility": 9,
+      "luck": 7,
+      "vitality": 5,
+      "strength": 2,
+      "wisdom": 3,
+      "intelligence": 3
+    }
+  },
+  {
+    "id": "legendary_weapon_1",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Thunderstrike",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "vitality": 9,
+      "intelligence": 12,
+      "luck": 9,
+      "wisdom": 6,
+      "agility": 3,
+      "strength": 4
+    }
+  },
+  {
+    "id": "legendary_gloves_2",
+    "type": "equipment",
+    "category": "gloves",
+    "name": "Iron Grasp",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "vitality": 9,
+      "intelligence": 6,
+      "luck": 9,
+      "agility": 7,
+      "strength": 7,
+      "wisdom": 9
+    }
+  },
+  {
+    "id": "legendary_leggings_3",
+    "type": "equipment",
+    "category": "leggings",
+    "name": "Legguards of the Phoenix",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "wisdom": 5,
+      "intelligence": 12,
+      "agility": 10,
+      "luck": 5,
+      "vitality": 9,
+      "strength": 8
+    }
+  },
+  {
+    "id": "legendary_helmet_4",
+    "type": "equipment",
+    "category": "helmet",
+    "name": "Crown of the Phoenix",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "strength": 13,
+      "intelligence": 12,
+      "luck": 7,
+      "agility": 1,
+      "vitality": 4,
+      "wisdom": 7
+    }
+  },
+  {
+    "id": "legendary_weapon_5",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Blade of eternity",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "wisdom": 9,
+      "strength": 11,
+      "intelligence": 8,
+      "luck": 9,
+      "agility": 9,
+      "vitality": 4
+    }
+  },
+  {
+    "id": "legendary_helmet_6",
+    "type": "equipment",
+    "category": "helmet",
+    "name": "Helm of Eternity",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "agility": 7,
+      "intelligence": 10,
+      "vitality": 6,
+      "luck": 7,
+      "strength": 6,
+      "wisdom": 11
+    }
+  },
+  {
+    "id": "legendary_weapon_7",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Embercleave",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "vitality": 9,
+      "intelligence": 9,
+      "strength": 7,
+      "agility": 6,
+      "wisdom": 6,
+      "luck": 3
+    }
+  },
+  {
+    "id": "legendary_weapon_8",
+    "type": "equipment",
+    "category": "weapon",
+    "name": "Blade of Dawn",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "strength": 5,
+      "agility": 6,
+      "vitality": 9,
+      "intelligence": 11,
+      "wisdom": 7,
+      "luck": 9
+    }
+  },
+  {
+    "id": "legendary_boots_9",
+    "type": "equipment",
+    "category": "boots",
+    "name": "Boots of the Phoenix",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "vitality": 8,
+      "strength": 12,
+      "intelligence": 6,
+      "agility": 8,
+      "luck": 5,
+      "wisdom": 5
+    }
+  },
+  {
+    "id": "legendary_gloves_10",
+    "type": "equipment",
+    "category": "gloves",
+    "name": "Gauntlets of Might",
+    "rarity": "legendary",
+    "cost": 15000,
+    "stats": {
+      "intelligence": 11,
+      "wisdom": 10,
+      "luck": 10,
+      "agility": 7,
+      "vitality": 6,
+      "strength": 3
+    }
+  }
 ];
